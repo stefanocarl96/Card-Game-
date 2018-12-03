b397e8987b7c3b2caa182cb6760a34b833b249e1
fmaylinch <ferranmaylinch@gmail.com>
2018-12-03 15:23:04 +0100

    Util class (not finished)

---
+++ src/main/java/tech/bts/cardgame/util/Util.java
@@ -0,0 +1,16 @@
+package tech.bts.cardgame.util;
+
+import java.util.Collection;
+
+public class Util {
+
+    /** Returns a string with the texts combined with the separator in between each of them */
+    public static String join(Collection<String> texts, String separator) {
+
+        String result = "";
+
+        // TODO
+
+        return result;
+    }
+}
