3fe3d13097ce71011bb4c70c063d96b434c21bd0
fmaylinch <ferranmaylinch@gmail.com>
2018-12-10 09:57:29 +0100

    Changes to web controller / join game (hardcoded user)

--- src/main/java/tech/bts/cardgame/controller/GameWebController.java
+++ src/main/java/tech/bts/cardgame/controller/GameWebController.java
@@ -5,7 +5,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tech.bts.cardgame.model.Game;
+import tech.bts.cardgame.model.GameUser;
 import tech.bts.cardgame.service.GameService;
+
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -24,7 +29,20 @@ public class GameWebController {
     @RequestMapping(method = GET)
     public String getAllGames() {
 
-        return buildGameList();
+        String result = "<h1>List of games</h1>\n";
+
+        result += "<p><a href=\"/games/create\">Create game</a></p>\n";
+
+        result += "<ol>\n";
+
+        for (Game game : gameService.getAllGames()) {
+
+            result += "<li><a href=\"/games/" + game.getId() + "\">Game " + game.getId() + "</a> is " + game.getState() + "</li>\n";
+        }
+
+        result += "</ol>\n";
+
+        return result;
     }
 
     /** Returns details of the game with the given gameId (as HTML) */
@@ -33,33 +51,32 @@ public class GameWebController {
 
         Game game = gameService.getGameById(gameId);
 
-        return
+        String result =
+                "<a href=\"/games\">Go back to game list</a>" +
                 "<h1>Game " + game.getId() + "</h1>" +
                 "<p>State: " + game.getState() + "</p>" +
                 "<p>Players: " + game.getPlayerNames() + "</p>";
-    }
 
-    /** Creates a new game and returns the games (as HTML) */
-    @RequestMapping(method = GET, path = "/create")
-    public String createGame() {
-
-        gameService.createGame();
-
-        return buildGameList();
-    }
-
-    /** Builds the HTML to display the games */
-    private String buildGameList() {
-
-        String result = "<h1>List of games</h1>";
-
-        result += "<p><a href=\"/games/create\">Create game</a></p>";
-
-        for (Game game : gameService.getAllGames()) {
-
-            result += "<p><a href=\"/games/" + game.getId() + "\">Game " + game.getId() + "</a> is " + game.getState() + "</p>";
+        if (game.getState() == Game.State.OPEN) {
+            result += "<a href=\"/games/" + game.getId() + "/join\">Join game</a>";
         }
 
         return result;
     }
+
+    @RequestMapping(method = GET, path = "/{gameId}/join")
+    public void joinGame(HttpServletResponse response, @PathVariable long gameId) throws IOException {
+
+        gameService.joinGame(new GameUser(gameId, "Ferran"));
+        response.sendRedirect("/games/" + gameId);
+    }
+
+    /** Creates a new game and returns the games (as HTML) */
+    @RequestMapping(method = GET, path = "/create")
+    public void createGame(HttpServletResponse response) throws IOException {
+
+        gameService.createGame();
+        response.sendRedirect("/games");
+    }
+
 }
