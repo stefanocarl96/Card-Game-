886cacf559f0121486772f0245c8b2d455387eb0
stefanocarluccio96 <stefano.carluccio@bts.tech>
2019-02-27 16:34:52 +0100

    Create React Component

--- src/main/resources/public/Components/Article.js
+++ src/main/resources/public/Components/Article.js
@@ -2,6 +2,6 @@ const Article = (props) => (
     <div>
         <Title text={props.title}></Title>
         <p>{props.text}</p>
-        <Link text="Reload" url="http://localhost:8080/react-sample.html"/>
+        <Link text="Relod" url="http://localhost:8080/react-sample.html"/>
     </div>
 );
